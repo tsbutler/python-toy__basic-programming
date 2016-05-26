6341abc2d1949660825762cd12e4dbc276a20718
Sumeet Jain <sumeet@sumeetjain.com>
2016-05-26 10:03:16 -0500

    Polish

---
+++ test_3_add_up_numbers.py
@@ -0,0 +1,23 @@
+# NOTE: You may NOT use the `sum()` function for exercise. Use a loop.
+
+def add_up_numbers(arr):
+  # Remove the `return None` code below and add your own code.
+  return None
+
+# -----------------------------------------------------------------------------
+
+# These are the automated tests for this exercise. Do not modify them at all.
+# When you run `bin/check`, these tests will report on the validity of your
+# algorithm.
+
+def test_adds_two_numbers():
+  assert add_up_numbers([3, 10]) == 13
+
+def test_adds_three_numbers():
+  assert add_up_numbers([20, 12, 51]) == 83
+
+def test_handles_single_number():
+  assert add_up_numbers([42]) == 42
+
+def test_lots_of_numbers():
+  assert add_up_numbers([23, 31, 89, 21, 2, 39, 202, 230, 142, 14]) == 793
---
+++ test_4_filter_numbers.py
@@ -0,0 +1,21 @@
+def filter_numbers(arr):
+  # Remove the `return None` code below and add your own code.
+  return None
+
+# -----------------------------------------------------------------------------
+
+# These are the automated tests for this exercise. Do not modify them at all.
+# When you run `bin/check`, these tests will report on the validity of your
+# algorithm.
+
+def test_removes_92():
+  assert filter_numbers([3, 92]) == [3]
+
+def test_removes_10_and_greater():
+  assert filter_numbers([30, 5, 9, 10, 11]) == [5, 9]
+
+def test_removes_big_numbers_including_decimals():
+  assert filter_numbers([50, 49.5, 30.00, 5, 6, 0, 10]) == [5, 6, 0]
+
+def test_removes_no_numbers():
+  assert filter_numbers([3, 6, 7, 0, 2]) == [3, 6, 7, 0, 2]
